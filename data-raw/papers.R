579c8dac7be92363c5847b28d2c036edf00aa847
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-12-19 11:26:20 -0800

    fixed

--- data-raw/papers.R
+++ data-raw/papers.R
@@ -1,18 +1,15 @@
-
 devtools::load_all()
 
 library(rcrossref)
 
-dois <- read.csv("papers/DOIs.csv") |> 
-  pull(DOI) 
+dois <- read.csv("papers/DOIs.csv") |>
+  pull(DOI)
 
-bibtex_entries <- 
-  cr_cn(dois, format = "bibtex") |> 
-  unlist() |> 
+bibtex_entries <-
+  cr_cn(dois, format = "bibtex") |>
+  unlist() |>
   stringr::str_trim(side = "left")
 bibtex_entries |>
   cat(file = "publications.bib")
 
-# usethis::use_data(papers, overwrite = TRUE)
 bibtex_entries |> create_pub_listing()
-
