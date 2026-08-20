5e260e4493237a92d3d52690e8dee56cb32f6b28
Kristen Aiemjoy <16113030+kaiemjoy@users.noreply.github.com>
2026-08-20 12:12:59 +0700

    fixed dois, and changed create_pup_listing.r function to render dates in quotations. this fixed the issue where some dates were being rendered as "January 1970"

--- R/create_pub_listing.R
+++ R/create_pub_listing.R
@@ -25,6 +25,13 @@ create_pub_listing <- function(bib = bibtex::read.bib(bib_file),
         -3
       )
 
+      # Quote issued dates so YAML does not interpret year-only values as numbers
+      article <- article |>
+        stringr::str_replace(
+          "^(\\s*issued:\\s*)(\\d{4}(?:-\\d{2}(?:-\\d{2})?)?)\\s*$",
+          '\\1"\\2"'
+        )
+      
       article <- c(
         article,
         grep("  container-title:", article, value = TRUE) |>
